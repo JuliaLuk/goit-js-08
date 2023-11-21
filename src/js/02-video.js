d16b431836fbbc87ae2569fb42b83d34adde833b
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-11-21 09:36:09 -0600

    dz 8 (02 i 03)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+//const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player('vimeo-player');
+const key = 'videoplayer-time';
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(key, data.seconds);
+  }, 1000)
+);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const saveTime = localStorage.getItem(key);
+  if (saveTime) {
+    player.setCurrentTime(saveTime);
+  }
+});
